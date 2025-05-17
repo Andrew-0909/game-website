f8422433ced3ad8482f259ec6a99f7ab0eb12149
SCAR 17 SE G733CX <SCAR17SEG733CX@mac.mynetworksettings.com>
2025-05-17 15:20:14 -0700

    rating

---
+++ signup.js
@@ -0,0 +1,28 @@
+document.getElementById("form").addEventListener("submit", function(event){
+    event.preventDefault();
+const username = document.getElementById("username").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+if(!username||!email||!password){
+document.getElementById("signupMessage").textContent = "Something went wrong. Please fill out every box."
+return;
+}
+const users = JSON.parse(localStorage.getItem("users")||"{}");
+if(users[email]){
+document.getElementById("signupMessage").textContent = "Someone is already using this email."
+} else if(users[username]){
+document.getElementById("signupMessage").textContent = "Someone is already using this username."
+} else{
+    users[email] = {
+        username:username, 
+        password:password
+    };
+    localStorage.setItem("users",JSON.stringify(users));
+    localStorage.setItem("currentUser", username);
+    document.getElementById("signupMessage").textContent = "Sign up complete!"
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 2000);
+}
+});
+
