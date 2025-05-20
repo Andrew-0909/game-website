cd44476557e7d31ffd06c24b5fda0ea5e7bbfc9e
SCAR 17 SE G733CX <SCAR17SEG733CX@mac.mynetworksettings.com>
2025-05-19 18:38:20 -0700

    fixing the login page

--- login.js
+++ login.js
@@ -1,9 +1,10 @@
 document.getElementById("loginForm").addEventListener("submit", function(event){
     event.preventDefault();
-const loginMessage = document.getElementById("loginMessage").value;
+const loginMessage = document.getElementById("loginMessage");
 const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
 const username = document.getElementById("username").value;
+const users = JSON.parse(localStorage.getItem("users")||"{}");
 
 if(!users[email]){
 loginMessage.textContent = "You have not sign up with this email."
@@ -15,7 +16,7 @@ loginMessage.textContent = "The password is wrong, try again."
 loginMessage.style.color = "#cb0606";
 return;
 }
-localStorage.setItem("currentUser",username);
+localStorage.setItem("currentUser",email);
 loginMessage.textContent = "Login successful!"
 loginMessage.style.color = "#ffffff";
 setTimeout(()=>{
